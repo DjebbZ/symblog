166375ff82de4420ed308fc08b059f687e5ee0b7
DjebbZ <khalid.jebbari@gmail.com>
2013-07-11 09:05:48 +0200

    Create and display Contact form, no validator yet.

--- src/Djebbz/BlogBundle/Controller/PageController.php
+++ src/Djebbz/BlogBundle/Controller/PageController.php
@@ -3,6 +3,8 @@
 namespace Djebbz\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use  Djebbz\BlogBundle\Entity\Enquiry;
+use  Djebbz\BlogBundle\Form\EnquiryType;
 
 class PageController extends Controller
 {
@@ -14,5 +16,23 @@ class PageController extends Controller
     public function aboutAction()
     {
         return $this->render('DjebbzBlogBundle:Page:about.html.twig');
+    }
+
+    public function contactAction()
+    {
+        $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+        $request = $this->getRequest();
+
+        if ($request->getMethod() == 'POST') {
+            $form->bindRequest($request);
+
+            if ($form->isValid()) {
+                return $this->redirect($this->generateUrl('DjebbzBlogBundle_contact'));
+            }
+        }
+        return $this->render('DjebbzBlogBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
