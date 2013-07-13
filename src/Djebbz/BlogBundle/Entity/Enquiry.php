f85eb92372daafc9e52d08ca7ed16c81bf88ff3e
DjebbZ <khalid.jebbari@gmail.com>
2013-07-13 15:11:54 +0200

    Add validation rules to Enquiry Entity.

--- src/Djebbz/BlogBundle/Entity/Enquiry.php
+++ src/Djebbz/BlogBundle/Entity/Enquiry.php
@@ -2,6 +2,11 @@
 
 namespace Djebbz\BlogBundle\Entity;
 
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+
 class Enquiry
 {
     protected $name;
@@ -54,5 +59,17 @@ class Enquiry
     {
         $this->body = $body;
         return $this;
+    }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraint('name', new NotBlank());
+
+        $metadata->addPropertyConstraint('email', new Email());
+
+        $metadata->addPropertyConstraint('subject', new NotBlank());
+        $metadata->addPropertyConstraint('subject', new Length(array('max' => 50)));
+
+        $metadata->addPropertyConstraint('body', new Length(array('min' => 50)));
     }
 }
